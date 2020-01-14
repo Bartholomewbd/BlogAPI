7d331b0c5ce3041b79dff789fffe823b2292d6e7
Brian Bartholomew <brian.bartholomew@takeout7.com>
2020-01-13 23:58:41 -0500

    instial commit

---
+++ CommentAPI/Models/CommentItem.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CommentAPI.Models
+{
+    public class CommentItem
+    {
+        public long CommentId { get; set; }
+        public string PostSlug { get; set; }
+        public string CommentTitle { get; set; }
+        public DateTime CommentTime { get; set; }
+        public string CommentBody { get; set; }
+    }
+}
